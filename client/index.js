611c2b67588a828023a6b2e220fb318be627cbbd
Sardar-Ali-Murad <mar389786@gmail.com>
2024-11-11 19:33:13 +0500

    changes

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const uri =
-  "mongodb+srv://Hyphen:akRCsdjQPym3B5hJ@hyphen.vvshgb9.mongodb.net/sample_mflix?retryWrites=true&w=majority";
+  "hyphen-mongodb-url";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
